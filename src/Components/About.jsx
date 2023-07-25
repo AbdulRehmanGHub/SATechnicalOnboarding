d6480e52db8ce09e6b2daf0f10af73d4e76ca4c6
Abdul Rehman <105493274+AbdulRehmanGHub@users.noreply.github.com>
2023-07-25 06:27:04 +0000

    changes to about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "My name is Abdul Rehman, and I am a student of Bachelor's degree in Computer Science, which is 4 years degree program. I'm currently enrolled in the 5th semester of my degree at the Virtual University of Pakistan. I am a passionate and hardworking student with great communications and team management skills..";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web Development",
+  "Content Creation",
+  "Programming",
+  "Communication",
+  "Leadership",
+  "Team Building",
 ];
 
 /**
