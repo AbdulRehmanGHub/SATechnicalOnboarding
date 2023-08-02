aa2e4e6dfe29fb8301cfd939a3a56d6f3a2a7b1d
Abdul Rehman <105493274+AbdulRehmanGHub@users.noreply.github.com>
2023-08-02 10:32:00 +0500

    added changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/computer.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "laptop and study pic for portfolio";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Product_Landing_Page",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Smart Watch Landing Page Website",
+    url: "https://abdulrehmanghub.github.io/Product_Landing_Page/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Calculator_App",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A Beautiful Calculator App",
+    url: "https://abdulrehmanghub.github.io/LGMVIP-Web-Task2_Calculator_App/",
   },
   {
-    title: "My Resume Site",
+    title: "My Profile Page",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Abdul Rehman portfolio website",
+    url: "https://abdulrehmanghub.github.io/My-Portfolio/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Live-clock-and-Date-site",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Real Time date and time showing website",
+    url: "https://abdulrehmanghub.github.io/Live-clock-and-Date-site/",
   },
 ];
 
